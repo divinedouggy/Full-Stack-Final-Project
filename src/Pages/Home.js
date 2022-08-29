e867177d0a6e2e034931402d7e8dab81e8b99e9c
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-08-28 21:29:08 -0400

    more stylin

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,9 +3,7 @@
 function Home() {
     return (
         <div className="home">
-            <p> Welcome to YouZoo!</p>
-            <p> This is place where you can look at all kinds of animals</p>
-            <p> Also you can...</p>
+            <h1> Welcome to YouZoo!</h1>
         </div>
     )
 }
