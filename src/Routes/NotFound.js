8bac8667603da28eb80f2477bf96ac27196968a4
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-08-30 23:47:44 -0400

    NotFound page

--- src/Routes/NotFound.js
+++ src/Routes/NotFound.js
@@ -1,9 +1,12 @@
-
+import image from '../Images/facepalm-gorilla.jpg'
+import bunny from '../Images/facepalm-bunny.png'
 
 function NotFound() {
     return(
-        <div>
-            Error 404
+        <div className='not-found'>
+            <h1>Page Not Found</h1>
+            <img src={bunny} alt="Gorilla Facepalming"/>
+            <p>Sorry! It would seem this page doesn't exist!</p> 
         </div>
     )
 }
